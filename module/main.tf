c862c81036aaa3e01ecdb91d8710d71f80bded26
sreedhar <sreedhar70755@gmail.com>
2023-08-20 12:53:29 +0530

    tf

--- module/main.tf
+++ module/main.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "main" {
   for_each = var.subnets
   vpc_id     = var.vpc_id
-  cidr_block = var.subnets["cidr"]
+  cidr_block = each.value["cidr"]
 
   tags = {
     Name = each.key
