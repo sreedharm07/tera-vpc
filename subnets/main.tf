bd88d938be3723eda40044aa291ed74455453ab9
sreedhar <sreedhar70755@gmail.com>
2023-08-20 10:39:59 +0530

    tf

--- subnets/main.tf
+++ subnets/main.tf
@@ -19,8 +19,8 @@ resource "aws_route_table" "route" {
 
 resource "aws_route_table_association" "a" {
   for_each = var.subnets
-  subnet_id      = aws_subnet.main.id
-  route_table_id = lookup(lookup(aws_route_table,each.value,null ),"id" , null)
+  subnet_id      = lookup(lookup(aws_subnet.main,each.value , null), "id", null)
+  route_table_id = lookup(lookup(aws_route_table.route,each.value,null ),"id" , null)
 #  route_table_id= aws_route_table.route
 }
 
