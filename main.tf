81205cb4231f38cec93750a78364c3c6148efc90
sreedhar <sreedhar70755@gmail.com>
2023-08-21 21:22:02 +0530

    tf

--- main.tf
+++ main.tf
@@ -28,3 +28,13 @@ resource "aws_route" "igw" {
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.igw.id
 }
+
+resource "aws_eip" "id" {
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  for_each = lookup(lookup(module.subnets,"public", null ),"subnets",null)
+  allocation_id = aws_eip.id.id
+  subnet_id     = each.value["id"]
+}
