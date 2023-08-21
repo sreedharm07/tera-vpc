775eda169ecad6c0c7a9dce24b5cab78a38e61ac
sreedhar <sreedhar70755@gmail.com>
2023-08-21 21:06:14 +0530

    tf

--- main.tf
+++ main.tf
@@ -14,10 +14,17 @@ output "subnets" {
   value = module.subnets
 }
 
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 
   tags = {
     Name = "internet_public"
   }
+}
+
+resource "aws_route" "igw" {
+  for_each = lookup(lookup(module.subnets,each.key,null ),"public" ,null)
+  route_table_id            = each.value["id"]
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
 }
