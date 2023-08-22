f8f2267032597bee9a539f92719dcdcef07164ce
sreedhar <sreedhar70755@gmail.com>
2023-08-22 11:22:06 +0530

    tf

--- main.tf
+++ main.tf
@@ -19,3 +19,9 @@ resource "aws_internet_gateway" "igw" {
     Name = "internot"
   }
 }
+
+resource "aws_route" "igw" {
+  for_each       = lookup(lookup(module.subnets, "public", null), "route", null)
+  route_table_id = lookup(each.value, "id", null)
+  gateway_id     = aws_internet_gateway.igw.id
+}
