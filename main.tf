a01919c3e19f579da7650f01d2cd5c1d36596eb4
sreedhar <sreedhar70755@gmail.com>
2023-08-22 12:11:19 +0530

    tf

--- main.tf
+++ main.tf
@@ -24,9 +24,9 @@ output "subnets" {
   value = module.subnets
 }
 
-resource "aws_route" "igw" {
-  for_each               =lookup(lookup(module.subnets,"public",null),"route",null)
-  route_table_id         =each.value["id"]
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
+#resource "aws_route" "igw" {
+#  for_each               =lookup(lookup(module.subnets,"public",null),"route",null)
+#  route_table_id         =each.value["id"]
+#  destination_cidr_block = "0.0.0.0/0"
+#  gateway_id             = aws_internet_gateway.igw.id
+#}
