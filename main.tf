749af87b4bc4e73a8bd32cf3faab324048250ae7
sreedhar <sreedhar70755@gmail.com>
2023-08-22 14:36:31 +0530

    tf

--- main.tf
+++ main.tf
@@ -98,3 +98,9 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
+resource "aws_instance" "instance" {
+  ami = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  subnet_id = local.app_subnets[0]
+}
