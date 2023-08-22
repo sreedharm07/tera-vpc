b5c404583e870a442f5085875c4ec3e055cda2ad
sreedhar <sreedhar70755@gmail.com>
2023-08-22 10:20:34 +0530

    tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "vpc" {
-  cidr_block = var.vpc
+  cidr_block = var.vpc_id
 }
 
 
--- module/main.tf
+++ module/main.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "main" {
   for_each          = var.subnets
-  vpc_id            = var.vpc
+  vpc_id            = var.vpc_id
   cidr_block        = each.value["cidr"]
   availability_zone = each.value["az"]
 
@@ -9,5 +9,5 @@ resource "aws_subnet" "main" {
   }
 }
 
-variable "vpc" {}
+variable "vpc_id" {}
 variable "subnets" {}
